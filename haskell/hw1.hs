dfe39fdc813f964ee79af71fec2e908beb3380be
harry2262 <jashanbath608@gmail.com>
2025-07-18 23:26:44 +0530

    solved first part of hw1

---
+++ haskell/hw1.hs
@@ -0,0 +1,32 @@
+toDigits :: Integer -> [Integer]
+toDigits 0 = []
+toDigits x = x `mod` 10 : toDigits (x `div` 10)
+
+reverseList :: [Integer] -> [Integer]
+reverseList [] = []
+reverseList (x : []) = [x]
+reverseList (x : xs) = reverseList xs ++ [x]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther x
+  | (length x) `mod` 2 == 0 = doubleEveryOtherEven x
+  | otherwise = doubleEveryOtherOdd x
+
+doubleEveryOtherOdd :: [Integer] -> [Integer]
+doubleEveryOtherOdd (x : []) = [x]
+doubleEveryOtherOdd (x : (y : ys)) = (x : (2 * y : doubleEveryOtherOdd ys))
+
+doubleEveryOtherEven :: [Integer] -> [Integer]
+doubleEveryOtherEven [] = []
+doubleEveryOtherEven (x : (y : ys)) = (2 * x : (y : doubleEveryOtherEven ys))
+
+sumList :: [Integer] -> Integer
+sumList [] = 0
+sumList (x : xs) = x + sumList xs
+
+sumListDigit :: [Integer] -> Integer
+sumListDigit [] = 0
+sumListDigit (x : xs) = sumList (toDigits x) + sumListDigit xs
+
+validate :: Integer -> Bool
+validate x = (sumListDigit (doubleEveryOther (reverseList (toDigits x)))) `mod` 10 == 0
