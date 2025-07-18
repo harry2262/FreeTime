c7e033e4ccac5ec243bf15f22ff3896ed3eae804
harry2262 <jashanbath608@gmail.com>
2025-07-18 23:40:28 +0530

    refactor

--- haskell/hw1.hs
+++ haskell/hw1.hs
@@ -4,21 +4,22 @@ toDigits x = x `mod` 10 : toDigits (x `div` 10)
 
 reverseList :: [Integer] -> [Integer]
 reverseList [] = []
-reverseList (x : []) = [x]
+reverseList [x] = [x]
 reverseList (x : xs) = reverseList xs ++ [x]
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther x
-  | (length x) `mod` 2 == 0 = doubleEveryOtherEven x
+  | even (length x) = doubleEveryOtherEven x
   | otherwise = doubleEveryOtherOdd x
 
 doubleEveryOtherOdd :: [Integer] -> [Integer]
-doubleEveryOtherOdd (x : []) = [x]
-doubleEveryOtherOdd (x : (y : ys)) = (x : (2 * y : doubleEveryOtherOdd ys))
+doubleEveryOtherOdd [x] = [x]
+doubleEveryOtherOdd (x : (y : ys)) = x : (2 * y : doubleEveryOtherOdd ys)
 
 doubleEveryOtherEven :: [Integer] -> [Integer]
 doubleEveryOtherEven [] = []
-doubleEveryOtherEven (x : (y : ys)) = (2 * x : (y : doubleEveryOtherEven ys))
+
+doublEveryOtherEven (x : (y : ys)) = (2 * x : (y : doubleEveryOtherEven ys))
 
 sumList :: [Integer] -> Integer
 sumList [] = 0
@@ -29,4 +30,4 @@ sumListDigit [] = 0
 sumListDigit (x : xs) = sumList (toDigits x) + sumListDigit xs
 
 validate :: Integer -> Bool
-validate x = (sumListDigit (doubleEveryOther (reverseList (toDigits x)))) `mod` 10 == 0
+validate x = sumListDigit (doubleEveryOther (reverseList (toDigits x))) `mod` 10 == 0
